871bd01562fdd11316da66f04a1d8e312f637c9e
Gavra <gavra90@gmail.com>
2014-11-04 21:17:24 +0100

    first commit before adding entity framework

---
+++ RecoMovie/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecoMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PretragaFilmova(string query)
+        {
+
+          //  return Json(new {title="Movei title", id=4}, JsonRequestBehavior.AllowGet);
+
+
+
+            return Json(new
+            {
+                movies = new[]{new {title="Movei title", id=4},
+                                            new {title="Movei title 2", id=5}}
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ RecoMovie/Scripts/app.js
@@ -0,0 +1,54 @@
+﻿$(document).ready(function () {
+
+    $("#pretraga").on('click', function () {
+
+        alert($("#movies").val());
+
+    });
+
+    $('#chc_horor').prettyCheckable({
+        label:'Horror'
+    });
+    $('#chc_triler').prettyCheckable({
+        label: 'Thriller'
+    });
+    $('#chc_commedy').prettyCheckable({
+        label: 'Comedy'
+    });
+
+  
+    $("#movies").autocomplete({
+        minLength: 2,
+        source: function (request, response) {
+            $.ajax({
+                url: "/Home/PretragaFilmova",
+                dataType: "JSON",
+                data: {
+                    query: request.term
+                },
+                success: function (data) {
+                 //   alert("asd");
+                    response($.map(data.movies, function (item) {
+                        return {
+                            label: item.title,
+                            value: item.title,
+                            id:item.id
+                        }
+                    }));
+                },
+                error: function () {
+                    alert("GRESKAA!");
+                }
+            });
+        },
+        select: function (event, ui) {
+          //  alert(ui.item.id);
+        }
+       
+    });
+
+
+
+
+
+});//end doc ready
